5386af3df7d27d548351f07a047ac58ea2381100
Ethan <ethanlyon@gmail.com>
2018-03-06 06:26:44 -0500

    removed gsutil

--- start_flask.sh
+++ start_flask.sh
@@ -3,6 +3,6 @@ APP_NAME=$2
 
 cd /home/$USER/
 mkdir $APP_NAME
-gsutil cp -r /home/$USER/python-docs-samples/appengine/standard/flask/* /home/$USER/$APP_NAME
+cp -Ri /home/$USER/python-docs-samples/appengine/standard/flask/* /home/$USER/$APP_NAME
 
 #TO DO: 1) Check if python docs samples is installed
