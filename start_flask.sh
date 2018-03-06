960e43793e4f2f97a93cf956ef7531e84e51aaeb
Ethan <ethanlyon@gmail.com>
2018-03-06 06:01:40 -0500

    initial commit

---
+++ start_flask.sh
@@ -0,0 +1,8 @@
+USER=$1
+APP_NAME=$2
+
+cd /home/$USER/
+mkdir $APP_NAME
+gsutil cp -r /home/$USER/python-docs-samples/appengine/standard/flask/* /home/$USER/$APP_NAME
+
+#TO DO: 1) Check if python docs samples is installed
